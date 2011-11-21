05c6e2268c41fc74e91e137e450bde4995e0db51
damian <damian@frey.NOSPAMco.nz>
2011-11-21 20:56:44 +0100

    finished

--- src/testApp.h
+++ src/testApp.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxOpenCv.h"
+#include "ofxOsc.h"
 
 class testApp : public ofBaseApp{
 
@@ -19,4 +21,29 @@ class testApp : public ofBaseApp{
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
 		
+	
+	ofVideoGrabber cams[2];
+	
+	ofxCvColorImage working[2];
+
+	
+	ofxCvFloatImage accum[2];
+	
+	ofxCvGrayscaleImage diff_8bit[2];
+	ofxCvGrayscaleImage curr_8bit[2];
+	
+	
+	float max_act[2];
+	float min_act[2];
+	
+	float out_act[2];
+	
+	float low_quartile[2], high_quartile[2];
+	
+	bool snap_background[2];
+	
+	string osc_host;
+	int osc_port;
+	
+	ofxOscSender osc_sender;
 };
